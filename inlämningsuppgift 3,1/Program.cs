e9c821b33dfb124988fbd1d1b61d4162e3fefacc
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-11-11 14:26:56 +0100

    Add project files.

---
+++ inlämningsuppgift 3,1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du? (år)");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16)
+            {
+                Console.WriteLine("du får delta i tävlingen");
+            }
+           else if (ålder < 19)
+            {
+                Console.WriteLine("du får inte delta i tävlingen");
+            }
+        }
+    }
+}
